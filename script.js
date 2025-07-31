75bc5b90c1733c33c118c56756b4e12e4f935bc5
[Bence] <[nemetdbence@gmail.com]>
2025-07-31 12:13:03 +0300

    Login popup v1 done

---
+++ script.js
@@ -0,0 +1,42 @@
+//Selectors
+const login = document.querySelector('#login');
+const overlay = document.querySelector('#overlay');
+const content = document.querySelector('#content');
+
+//Global variables
+let users = [{username: 'Munlaly', password: 'Asd'}];
+
+
+//Event listeners
+login.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const usernameField = login.querySelector('#username');
+    const passwordField = login.querySelector('#password');
+
+    const username = usernameField.value.trim();
+    const password = passwordField.value;
+
+    const validUser = users.find(user => user.username === username && user.password === password);
+
+    if(!validUser){
+        alert('Username and password don\'t match!');
+    }
+    else{
+        alert('Successful login!');
+        login.classList.add('hidden');
+        overlay.classList.add('hidden');
+        content.hidden = false;
+    }
+});
+
+login.querySelector('#show-password-icon').addEventListener('click', () => {
+    const passwordField = login.querySelector('#password');
+    if(passwordField.type === 'password') passwordField.type = 'text';
+    else passwordField.type = 'password';
+})
+
+
+//Page loads
+document.addEventListener('DOMContentLoaded', () => {
+    content.hidden = true;
+})
